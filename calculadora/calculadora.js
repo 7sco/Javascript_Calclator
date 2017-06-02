a01d34031cfb4b1c1c0cf4aab0654efa32c10f7e
7sco <ISCO@Franciscos-MacBook-Pro.local>
2017-06-02 12:55:21 -0400

    Fixed issues where result would not appear

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -3,19 +3,18 @@
 
 
 function suma(){
+	var numero1 = parseFloat(document.getElementById('numero1').value);
+	var numero2 = parseFloat(document.getElementById('numero2').value);
+	var resultado= document.getElementById('resultado').value;
 	
-	if (document.getElementById('resultado').value=="") {
-		
-		document.getElementById('resultado')="Enter #s";
-		console.log('false');
 
+
+	if (!numero1 || !numero2) {
+		
+		console.log("Fill all Fields");
 	}
 
 	else{
-		var numero1 = parseFloat(document.getElementById('numero1').value);
-		var numero2 = parseFloat(document.getElementById('numero2').value);
-		var resultado= document.getElementById('resultado').value;
-	
 	
 		var resultado = numero1+numero2;
  	
